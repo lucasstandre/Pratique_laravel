8ea83c4e24e5691aedad2e9980a507ac8f2ba4e1
Lucas <lucas.st.andre@gmail.com>
2026-09-08 17:33:52 -0400

    chore(Workflows): initialisation du workflow pour le projet Laravel

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,8 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+        $response->assertSeeText("Let's get started");
+        $response->assertSeeText("Let's get started");
 
-        $response->assertStatus(200);
     }
 }
